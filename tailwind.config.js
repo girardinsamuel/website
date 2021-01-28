b9e2302ae669fcdf6be695744f581b3e39781572
Samuel Girardin <samuelgirardin@pm.me>
2021-01-28 21:38:32 +0100

    start designing home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: {
+    enabled: true,
+    // content: ['./src/**/*.html'],
+    content: ['./index.html'],
+  },
+  darkMode: "media", // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      mono: ['Roboto Mono', 'Arial', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
